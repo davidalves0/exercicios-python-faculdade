8bdcc93a63a4f994c29849061d6d1d57f95a4c4e
David Alves da Silva <davidmainsilva@gmail.com>
2026-08-30 22:30:57 -0300

    Py didnt upload, fixed the issue 4

---
+++ exer07_volumeparalelepipedo.py
@@ -0,0 +1,5 @@
+comprimento = float(input("Digite o comprimento do paralelepípedo: "))
+largura = float(input("Digite a largura do paralelepípedo: "))
+altura = float(input("Digite a altura do paralelepípedo: "))
+volumeParalelepipedo = comprimento * largura * altura
+print("O volume deste paralelepípedo é de", volumeParalelepipedo, "m3.")     
