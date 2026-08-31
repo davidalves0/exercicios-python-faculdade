b436ecc2335ee6ae4c72e9b858d73c311b7f24ae
David Alves da Silva <davidmainsilva@gmail.com>
2026-08-30 22:31:16 -0300

    Py didnt upload, fixed the issue 5

---
+++ exer11_comprimentocircunferencia.py
@@ -0,0 +1,3 @@
+raio = float(input("Digite o valor do raio da circunferência: "))
+comprimentoCircunferencia = 2 * 3.14 * raio
+print("O comprimento desta circunferência é de", comprimentoCircunferencia, "unidades")
