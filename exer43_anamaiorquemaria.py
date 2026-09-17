7eec99e752f14511577ab8892e1fec56e793014e
David Alves da Silva <davidmainsilva@gmail.com>
2026-09-17 14:03:17 -0300

    Segundo lote de exercicios

---
+++ exer43_anamaiorquemaria.py
@@ -0,0 +1,10 @@
+altura_ana = 1.10
+altura_maria = 1.50
+anos = 0
+
+while altura_ana <= altura_maria:
+    altura_ana = altura_ana + 0.03
+    altura_maria = altura_maria + 0.02
+    anos = anos + 1
+
+print(anos)
