ebaac9b52cdc4d9a1ecaaf0f000cfb8911981e9f
David Alves da Silva <davidmainsilva@gmail.com>
2026-09-17 14:03:01 -0300

    Segundo lote de exercicios

---
+++ exer42_serienum03.py
@@ -0,0 +1,10 @@
+numerador = 1
+denominador = 1
+soma = 0
+
+while numerador <= 50:
+    soma = soma + (numerador / denominador)
+    numerador = numerador + 1
+    denominador = denominador + 2
+
+print(soma)
